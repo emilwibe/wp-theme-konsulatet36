5cbf6670a28ba8b3d2264900258d5bf0960eef17
Emil Wibe <mail@emilwibe.dk>
2025-07-19 20:49:05 +0200

    [ul]

--- single-event-36.php
+++ single-event-36.php
@@ -1,9 +1,25 @@
-<?php
-if (!defined('ABSPATH')) { exit; }
-
-?>
 <?php get_header(); ?>
 
-event-36 single
+<?php if (have_posts()) : ?>
+
+    <main class="l-wrapper xl">
+
+            <?php while (have_posts()) : the_post(); ?>
+
+                <?php if (!is_front_page()) : ?>
+
+                    <h1><?php the_title(); ?></h1>
+
+                <?php endif; ?>
+
+                <div class="content">
+                    <?php the_content(); ?>
+                </div>
+
+            <?php endwhile; ?>
+
+    </main>
+
+<?php endif; ?>
 
 <?php get_footer(); ?>
